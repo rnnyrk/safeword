9c8f9d8cf81af5c97a15b09ad37965e117a3fb80
Ronny <4519142+rnnyrk@users.noreply.github.com>
2023-06-05 15:57:31 +0200

    Use svg instead of Image in LogoHeader

--- src/components/common/layout/LogoHeader/index.tsx
+++ src/components/common/layout/LogoHeader/index.tsx
@@ -1,7 +1,6 @@
-import { Image } from 'expo-image';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import SafeWordLogo from 'images/splash.png';
+import { Safeword } from 'common/svg';
 
 import { LogoHeaderContainer } from './styled';
 
@@ -9,12 +8,8 @@ export const LogoHeader = () => {
   const insets = useSafeAreaInsets();
 
   return (
-    <LogoHeaderContainer paddingTop={insets.top}>
-      <Image
-        source={SafeWordLogo}
-        contentFit="contain"
-        style={{ width: 200, height: 22, marginTop: 18 }}
-      />
+    <LogoHeaderContainer paddingTop={insets.top + 20}>
+      <Safeword width={200} />
     </LogoHeaderContainer>
   );
 };
