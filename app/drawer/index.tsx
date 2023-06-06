06202f157a0e85f83827c3b77277d6e488396cb7
Ronny <4519142+rnnyrk@users.noreply.github.com>
2023-06-06 11:10:25 +0200

    WIP drawer component

---
+++ app/drawer/index.tsx
@@ -0,0 +1,7 @@
+import { Drawer } from 'common/interaction/Drawer';
+
+// @TODO drawer should be on top level
+
+export default function DrawerLayout() {
+  return <Drawer />;
+}
--- src/components/common/svg/index.ts
+++ src/components/common/svg/index.ts
@@ -3,6 +3,7 @@ export * from './ArrowDown';
 export * from './ArrowRight';
 export * from './Bolt';
 export * from './Bubble';
+export * from './Menu';
 export * from './Lock';
 export * from './Safeword';
 export * from './Work';
