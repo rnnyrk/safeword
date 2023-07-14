96bbe6628a60d63822e9cd3be1bbbd09722dd2e0
Ronny Rook <info@ronnyrook.nl>
2023-07-14 15:32:01 +0200

    Add LexendDeca in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,36 @@
-import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import {
+  LexendDeca_400Regular,
+  LexendDeca_500Medium,
+  LexendDeca_800ExtraBold,
+  useFonts,
+} from '@expo-google-fonts/lexend-deca';
+import { SplashScreen, Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components';
 
 import theme from 'styles/theme';
 import { SupabaseProvider } from 'utils/SupabaseContext';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function AppLayout() {
+  const [fontsLoaded] = useFonts({
+    LexendDeca_400Regular,
+    LexendDeca_500Medium,
+    LexendDeca_800ExtraBold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
