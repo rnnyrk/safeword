d4db6f7334665fb4f3126b5e8fb140953813396a
Ronny Rook <info@ronnyrook.nl>
2023-11-07 13:55:37 +0100

    Create CustomScrollView, increaes regenerate code time

---
+++ src/components/common/layout/CustomScrollView/index.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+export const CustomScrollView = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    width: '100%',
+  },
+})`
+  width: 100%;
+`;
--- src/components/common/layout/index.ts
+++ src/components/common/layout/index.ts
@@ -1,6 +1,7 @@
 export * from './AnimatedGroup';
 export * from './Container';
 export * from './Countdown';
+export * from './CustomScrollView';
 export * from './DotLoader';
 export * from './FormLayout';
 export * from './LogoHeader';
